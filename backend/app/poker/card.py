e891e54777148bdc0c7b011c74674421f8b1ce7d
Colin McKay <colin.mckay2152006@gmail.com>
2026-07-30 15:43:32 -0400

    Initial commit

---
+++ backend/app/poker/card.py
@@ -0,0 +1,8 @@
+class Card:
+    def __init__(self, rank: str, suit: str):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self) -> str:
+        return f"{self.rank} of {self.suit}"
+    
